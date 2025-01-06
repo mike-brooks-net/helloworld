a8ea940ee11c4544b58887ce6be0ce178248136c
m.brooks.photo <m.brooks.photo@gmail.com>
2025-01-05 21:35:52 -0500

    adding unit tests

--- src/main/java/net/mikebrooks/hello/hello.java
+++ src/main/java/net/mikebrooks/hello/hello.java
@@ -1,4 +1,49 @@
+package net.mikebrooks.hello;
+
+
 public class hello {
+
+	public static int add(int x, int y) {
+		int retval; // I know we don't really need this...
+
+		// how stupid is it that the following lines were generated and I didn't have to do anything...
+
+		retval = x + y;
+		return retval;
+
+	}
+	public static int subtract(int x, int y) {
+		int retval; // I know we don't really need this...
+
+		// how stupid is it that the following lines were generated and I didn't have to do anything...
+
+		retval = x - y;
+		return retval;
+
+	}
+	public static int mul(int x, int y) {
+		int retval; // I know we don't really need this...
+
+		// how stupid is it that the following lines were generated and I didn't have to do anything...
+
+		retval = x * y;
+		return retval;
+
+	}
+
+	public static int div(int x, int y) {
+		int retval; // I know we don't really need this...
+
+		// how stupid is it that the following lines were generated and I didn't have to do anything...
+
+		retval = x / y;
+		return retval;
+
+	}
+
+
+
+
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!");
---
+++ src/test/java/net/mikebrooks/hello/helloTest.java
@@ -0,0 +1,34 @@
+package net.mikebrooks.hello;
+//import net.mikebrooks.hello.helloTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+class helloTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void Tadd() {
+        assertEquals(2, 2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void Tsubtract() {
+        assertEquals(2, 2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void Tmul() {
+        assertEquals(2, 2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void Tdiv() {
+        assertEquals(2, 2);
+    }
+}
